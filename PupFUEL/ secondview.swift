30f4557b3b18b846d521ca929d8078e3decd3380
Alex Railean <alexrailean06@gmail.com>
2023-07-17 17:29:21 +0300

    Create Second View

---
+++ PupFUEL/ secondview.swift
@@ -0,0 +1,20 @@
+//
+//   secondview.swift
+//  PupFUEL
+//
+//  Created by Apple on 17.07.2023.
+//
+
+import SwiftUI
+
+struct _secondview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct _secondview_Previews: PreviewProvider {
+    static var previews: some View {
+        _secondview()
+    }
+}
